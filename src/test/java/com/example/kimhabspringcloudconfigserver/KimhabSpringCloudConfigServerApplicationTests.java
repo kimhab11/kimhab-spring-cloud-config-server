be34071d27404af2ac3ece640d976292a2354006
kimhab <69948342+kimhab11@users.noreply.github.com>
2023-07-15 22:27:00 +0700

    15.07.23

---
+++ src/test/java/com/example/kimhabspringcloudconfigserver/KimhabSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kimhabspringcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimhabSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
