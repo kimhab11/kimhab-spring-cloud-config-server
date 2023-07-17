6f4f9fc65894fa0d0919359abae0b4f1f4762c3a
kimhab <69948342+kimhab11@users.noreply.github.com>
2023-07-17 14:02:18 +0700

    17.07.23

--- src/main/java/com/example/kimhabspringcloudconfigserver/TestController.java
+++ src/main/java/com/example/kimhabspringcloudconfigserver/TestController.java
@@ -1,16 +1,7 @@
 package com.example.kimhabspringcloudconfigserver;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @Value("${greeting.msg}")
-    private String greetingMessage;
-
-    @GetMapping("/greeting")
-    public String getGreetingMessage() {
-        return greetingMessage;
-    }
 }
